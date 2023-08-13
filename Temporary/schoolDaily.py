84751523215bae186ce5bfc434f54c673692256a
洪宇轩 <hongyuxuan@xysu.tech>
2023-08-13 11:08:42 +0800

    [临时]日常助手 - 获取学院公告

--- Temporary/schoolDaily.py
+++ Temporary/schoolDaily.py
@@ -32,5 +32,22 @@ def get_notice(num_of_notice):
             break
 
 
+def get_comp_notice(num_of_notice):
+    url = "http://xxjd.shnu.edu.cn/27065/list.htm"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    list_id = 0
+    news_list = soup.find_all("li", class_="news")
+    print("学院公告：")
+    for news in news_list:
+        list_id += 1
+        title = news.find("span", class_="news_title").find("a").text
+        date = news.find("span", class_="news_meta").text
+        print(f"{list_id}. [{date}] {title}")
+        if list_id >= num_of_notice:
+            break
+
+
 get_current_day()
 get_notice(5)
+get_comp_notice(5)
